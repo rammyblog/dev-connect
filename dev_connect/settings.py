fe4f87ac3551a9980bff5b06741844a22d9f540e
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-05-19 22:36:57 +0100

    Deploying requirements.txt

--- dev_connect/settings.py
+++ dev_connect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -139,12 +139,6 @@ TIME_ZONE = 'Africa/Lagos'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 CORS_ORIGIN_ALLOW_ALL = True
 
